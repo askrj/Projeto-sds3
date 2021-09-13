2af20d3f93b632c038db138b6a013b4dd7455fbf
Alex Santos <ask.rj10@gmail.com>
2021-09-13 15:07:39 -0300

    env var correcao

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,3 @@
-import BarChart from "components/BarChart";
-import DataTable from "components/DataTable";
-import DunutChart from "components/DunutChart";
-import Footer from "components/Footer";
-import NavBar from "components/navbar";
 import Routes from "Routes";
 
 function App() {
